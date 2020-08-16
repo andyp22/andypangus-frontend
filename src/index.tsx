6b4a1ec3d71925549b6d0a256c799ca8f08321e1
Andrew Page <andrew.page32@gmail.com>
2020-08-16 14:34:48 -0400

    Remove duplicate code

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,4 @@
-import ReactHabitat from "react-habitat";
-import "./components/App/App.scss";
-
-// Import container components
-import { ExamplePage } from "./components/ExamplePage/ExamplePage";
-
-// Define an array of containers with the HTML ID's to attach to
-const containers = [
-  {
-    id: "ExampleContainer",
-    component: ExamplePage,
-  },
-];
-
-class App extends ReactHabitat.Bootstrapper {
-  constructor() {
-    super();
-
-    // Create a new container builder:
-    const builder = new ReactHabitat.ContainerBuilder();
-
-    // Register container components
-    for (let container of containers) {
-      builder.register(container.component).as(container.id);
-    }
-
-    // Finally, set the container:
-    this.setContainer(builder.build());
-  }
-}
+import { App } from "./components/App/App";
 
 // Always export a 'new' instance so it immediately evokes:
 export default new App();
